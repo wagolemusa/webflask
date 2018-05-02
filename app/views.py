ed308a1bf045d470d2b146a320c421bcdaac9314
kisumutalent <homiemusa@gmail.com>
2018-05-02 07:51:55 +0300

    modifieding the app

--- app/views.py
+++ app/views.py
@@ -6,6 +6,12 @@ from app import app
 def index():
 	return render_template("index.html")
 
+
 @app.route('/about')
 def about():
 	return render_template("about.html")
+
+
+@app.route('/contact')
+def contact():
+	return render_template("contact.html")
