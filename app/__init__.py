bfc6f078f06da738dc8c00f52e4e3bf8ee2e3469
kisumutalent <homiemusa@gmail.com>
2018-05-02 09:26:56 +0300

    modified  __init__ file

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,21 @@
 
 from flask import Flask
 
-# Initialize the app
-app = Flask(__name__, instance_relative_config=True)
+from flask_sqlalchemy import SQLAlchemy
 
-# Load the views
-from app import views
 
-# Load the config file
-app.config.from_object('config')
+#local imports
+from config import app_config
+
+# db varibles intialization
+db = SQLAlchemy()
+
+def create_app(config_name):
+	app = Flask(__name__, instance_relative_config=True)
+	app.config.from_object(app_config[config_name])
+	app.config.from_pyfile('config.py')
+	db.init_app(app)
+
+
+	return app
+
