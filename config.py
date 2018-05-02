91e5291408c615dea3f16da815fd5714e04a1d53
kisumutalent <homiemusa@gmail.com>
2018-05-02 08:41:25 +0300

    configuring the config file

--- config.py
+++ config.py
@@ -1,5 +1,36 @@
 # config
 
-# Enable  Flask's debugging features. should be fasle in pruduction
 
-DEBUG = True
+# Enable  Flask's debugging features. should be fasle in pruduction
+#DEBUG = True
+
+class Config(object):
+	"""
+	common configurations
+
+	"""
+
+	# put any configuration  here that are common across all the enviroments
+
+class DelevelopmentConfig(Config):
+		"""
+		Delevepment configurations
+		"""
+
+		DEBUG = True
+		SQLALCHEMY_ECHO = True
+
+
+class ProductionConfig(Config):
+		"""
+			Production configurations
+
+		"""
+		DEBUG = False
+
+
+app_config = {
+	'development' : DelevelopmentConfig,
+	'production'  : ProductionConfig
+}
+
